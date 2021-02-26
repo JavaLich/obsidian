99dd4ca8aca356c7f2a66c0c14fa397dbde97452
Akash Melachuri <amelachuri@gmail.com>
2021-02-26 11:25:22 -0500

    Added gitignore, vulkano, and initialization code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+use vulkano::device::{Device, DeviceExtensions, Features};
+
 fn main() {
-    println!("Hello, world!");
+    let instance = Instance::new (None, &InstanceExtensions::none(), None)
+        .expect("Failed to create instance");
+
+    let physical = PhysicalDevice::enumerate(&instance).next()
+        .expect("No vulkan device available");
+
+    let queue_family = physical.queue_families()
+        .find(|&q| q.supports_compute())
+        .expect("Couldn't find compute queue family");
+
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+        [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+
+    let queue = queues.next().unwrap();
 }
