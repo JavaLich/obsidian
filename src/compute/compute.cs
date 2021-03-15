88def477dc5cd8c9f824a274b22b58bf498510ee
Akash Melachuri <amelachuri@gmail.com>
2021-03-15 08:30:22 -0400

    Calculated initial parameters for ray tracing

--- src/compute/compute.cs
+++ src/compute/compute.cs
@@ -32,9 +32,16 @@ void main() {
     uint y = gl_GlobalInvocationID.x / scene.width;
 
     float aspect_ratio = float(scene.width) / float(scene.height); 
+    float viewport_width = aspect_ratio * cam.viewport_height;
+    float viewport_height = cam.viewport_height;
 
-    if (x > 400) 
-        buf.data[x + scene.width * y] = 0x00ff00;
-    else 
-        buf.data[x + scene.width * y] = 0xffff00;
+    vec3 origin = vec3(0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+    vec3 lower_left = origin - horizontal/2 - vertical/2 - vec3(0, 0, cam.focal_length);
+
+    float u = float(x) / float(scene.width);
+    float v = float(y) / float(scene.height);
+
+    buf.data[x + y * scene.width] = 0xb3cfff;
 }
