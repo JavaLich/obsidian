22f59e42781674bbf9e72aa51fa1eba2bf852b8e
Akash Melachuri <amelachuri@gmail.com>
2021-04-03 12:21:05 -0400

    Added camera controls

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -15,8 +15,21 @@ pub fn run(tracer: &mut Tracer) {
 
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
+    let speed = 0.1;
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        tracer.change_camera_pos(-0.01, 0f32, 0f32);
+        if window.is_key_down(Key::W) {
+            tracer.change_camera_pos(0f32, 0f32, speed);
+        }
+        if window.is_key_down(Key::S) {
+            tracer.change_camera_pos(0f32, 0f32, -speed);
+        }
+        if window.is_key_down(Key::A) {
+            tracer.change_camera_pos(-speed, 0f32, 0f32);
+        }
+        if window.is_key_down(Key::D) {
+            tracer.change_camera_pos(speed, 0f32, 0f32);
+        }
 
         let pixels = tracer.compute();
 
